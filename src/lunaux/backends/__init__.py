0e824dae9745d6aeb65cf7e5117b2f94ceb84d0d
AnnonyA <79977221+AnnonyA@users.noreply.github.com>
2026-08-05 14:43:50 -0300

    Export automatic and reconstructed backends

--- src/lunaux/backends/__init__.py
+++ src/lunaux/backends/__init__.py
@@ -1,4 +1,13 @@
+from lunaux.backends.auto import AutoBackend, BackendMode, build_backend
 from lunaux.backends.base import DecompilerBackend
 from lunaux.backends.native import NativeModuleBackend
+from lunaux.backends.reconstructed import ReconstructedBackend
 
-__all__ = ["DecompilerBackend", "NativeModuleBackend"]
+__all__ = [
+    "AutoBackend",
+    "BackendMode",
+    "DecompilerBackend",
+    "NativeModuleBackend",
+    "ReconstructedBackend",
+    "build_backend",
+]
